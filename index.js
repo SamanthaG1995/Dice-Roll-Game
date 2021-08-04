f9be9f0b8460386c72ddfd271d873ef68f6f971c
Samantha Gunter <78177145+SamanthaG1995@users.noreply.github.com>
2021-08-04 00:32:01 +0000

    fixed small errors,  fixed the roll when  they get a 2 and worked on the start (s to start)

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 //explains the rules of the game
-console.log("Welcome to the Dice Rolling Game. Roll the digital dice and compete against your friend to try and get the highest dice roll. If you roll a 2 your point will be doubled. The player with the most six's will get an additional 2 points at the end of the game." );
-
+let start = prompt("Welcome to the Dice Rolling Game. Roll the digital dice and compete against your friend to try and get the highest dice roll. If you roll a 2 your point will be doubled. The player with the most six's will get an additional 2 points at the end of the game. Press q to quit. Press s to start" );
+if (start == 's'){
 //asks the player 1 to enter a name
 let player1 = prompt ('Player 1: Enter your name');
 //asks the user to re-enter their name if nothing is entered
 if (player1 == ''){
 console.log('That is an invalid input. Please enter a name');
-player1 = prompt('Player 1: Enter your name')
+let player1 = prompt('Player 1: Enter your name')
 }
 
 //asks player 2 to enter a name
@@ -14,7 +14,7 @@ let player2 = prompt ('Player 2: Enter your name');
 //asks the user to re-enter their name if nothing is entered
 if (player2 == ''){
 console.log('that is an invalid input. Please enter a name')
-player2 = prompt('Player 2: Enter your name')
+let player2 = prompt('Player 2: Enter your name')
 }
 //welcomes the user to the game
 console.log('Welcome ' + player1 + ' and ' + player2 + ' to the Dice Rolling Game!')
@@ -52,11 +52,12 @@ if(rollP1 == 1){
   pointP1++
 }
 if(rollP1 == 2){
-  console.log(player1 + 'got a 2! you get double points!')
+  console.log(player1 + ' got a 2! They get double points!')
   pointP1++
   pointP1++
   pointP1++
   pointP1++
+  rollP1 = 4
 }
 if(rollP1 == 3){
   pointP1++
@@ -90,6 +91,7 @@ if(rollP1 == 6) {
 }else{
   console.log('that is an invalid input. Please enter yes or no')
   rollingPlayer1 = prompt('would you like to roll the dice: yes or no:')
+  console.log('Ok your roll is ' + rollP1 + '!');
 }
 
 var rollingP2 = prompt (player2 + ' would you like to roll the dice?');
@@ -100,11 +102,12 @@ if(rollP2 == 1){
   pointP2++
 }
 if(rollP2 == 2){
-  console.log(player2 + 'got a 2! you get double points!')
+  console.log(player2 + ' got a 2! They get double points!')
   pointP2++
   pointP2++
   pointP2++
   pointP2++
+  rollP2 = 4
 }
 if(rollP2 == 3){
   pointP2++
@@ -138,12 +141,15 @@ if(rollP2 == 6) {
 }else{
   console.log('That is an invalid input. Please enter yes or no')
   rollingPlayer2 = prompt('Would you like to roll the dice: yes or no:')
+    console.log('Ok your roll is ' + rollP2 + '!');
 }
 
-if(numberP1 < numberP2){
+if(rollP1 < rollP2){
   console.log(player2 + ' won this round!')
-}else if(numberP1 > numberP2){
+}else if(rollP1 > rollP2){
   console.log(player1 + ' won this round!')
+}else{
+  console.log("It's a tie!")
 }
 round++
 console.log(player1 + ' has ' + pointP1 + ' points!')
@@ -156,6 +162,9 @@ console.log(player1 + ' wins the game!')
 }
 else if(pointP2 > pointP1){
   console.log(player2 + ' wins the game!')
+}else{
+  console.log("It was a tie!")
 }
-console.log(player1 + ' got ' + pointP1 + ' points')
-console.log(player2 + ' got ' + pointP2 + ' points')
+}else if(start = 'q'){
+  console.log('Ok thank you for playing!')
+}
